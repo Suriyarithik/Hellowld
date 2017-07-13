fd1acb526647b3516471febb1835c15a5c98d962
Suriyarithik <suriyavedsum@gmail.com>
2017-07-13 15:12:52 +0530

    Create Hellowld.java

---
+++ Hellowld.java
@@ -0,0 +1,15 @@
+# Hellowld
+import java.util.*;
+public class Hellowld
+{
+public static void main(String args[])
+{
+Scanner ss=new Scanner(System.in);
+String s=ss.nextLine();
+String[] ch=s.split(" ");
+for(int i=ch.length-1;i>=0;i--)
+{
+System.out.print(ch[i]+" ");
+}
+}
+}
